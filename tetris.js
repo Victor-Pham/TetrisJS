505b80591b20e14cd76fdc2e795c693798d2ab0f
Victor Pham <victorp11@outlook.com>
2019-04-08 14:44:36 -0400

    added gravity/rotation

--- tetris.js
+++ tetris.js
@@ -2,7 +2,7 @@ var tetris = {};
 
 //Draw the grid
 tetris.drawPlayField = function(){
-	for(var row=0;row<20;row++){
+	for(var row=0;row<22;row++){
 		$('#playfield').append('<tr class="'+row+'"></tr>');
 		for (var col=0;col<10;col++){
 			$('.'+row).append('<td id="'+col+'"></td>');
@@ -13,10 +13,38 @@ tetris.drawPlayField = function(){
 
 
 //Variable to store current coordiates
-tetris.currentCoor = [{row:1,col:1},
-    {row:1,col:2},
-    {row:2,col:1},
-    {row:2,col:2}];
+tetris.origin = {row:5, col:5};
+tetris.currentShape = 'L';
+tetris.currentCoor;
+
+tetris.shapeToCoor = function(shape, origin){
+    if(shape === 'L'){
+        return [{row: origin.row, col:origin.col},
+                {row:origin.row - 1, col:origin.col},
+                {row:origin.row + 1, col:origin.col},
+                {row:origin.row + 1, col:origin.col + 1}];
+    }
+    else if (shape === 'L90'){
+        return [{row:origin.row, col:origin.col},
+                {row:origin.row, col:origin.col + 1},
+                {row:origin.row, col:origin.col - 1},
+                {row:origin.row + 1, col:origin.col - 1}];
+    }
+    
+}
+
+tetris.rotate = function(){
+    this.fillCells(this.currentCoor, '');
+    if(this.currentShape === 'L'){
+        this.currentShape = 'L90';
+    }
+    else if (this.currentShape === 'L90'){
+        this.currentShape = 'L';
+    }
+    
+    this.currentCoor = this.shapeToCoor(this.currentShape, this.origin);
+    this.fillCells(this.currentCoor, 'orange');
+}
 
 
 //Fill the cells
@@ -33,22 +61,60 @@ tetris.fillCells = function(coordinates,fillColor){
 //Move shape
 tetris.move = function(direction){
     this.fillCells(this.currentCoor, '');
+
+    var reverse;
     for(var i = 0; i < this.currentCoor.length; i++){
         if(direction == 'right'){
             this.currentCoor[i].col++;
+            if(this.currentCoor[i].col > 9){
+                reverse = true;
+            }
         }
         else if (direction == 'left'){
             this.currentCoor[i].col--;
+            if(this.currentCoor[i].col < 0){
+                reverse= true;
+            }
         }
+        else if (direction == 'down'){
+            this.currentCoor[i].row++;
+            if(this.currentCoor[i].row > 21){
+                this.currentCoor[i].row = 0;
+            }
+        }
+        else if(direction =='up'){
+            this.currentCoor[i].row--;
+        }
+
     }
 
-    this.fillCells(this.currentCoor, 'yellow');
+    //move origin
+    if(direction === 'right'){
+        this.origin.col++;
+    }
+    else if (direction === 'left'){
+        this.origin.col--;
+    }
+
+    this.fillCells(this.currentCoor, 'orange');
+
+    if(reverse && direction === 'left'){
+        this.move('right');
+    }
+    else if (reverse && direction === 'right'){
+        this.move('left');
+    }
+
+
 }
+
+var gravity = setInterval(function(){tetris.move('down')}, 375);
 
 
 $(document).ready(function(){
     tetris.drawPlayField();
-    tetris.fillCells(tetris.currentCoor,'yellow');
+    tetris.currentCoor = tetris.shapeToCoor(tetris.currentShape, tetris.origin);
+    tetris.fillCells(tetris.currentCoor,'orange');
     
     $(document).keydown(function(e){
         console.log(e.keyCode);
@@ -58,6 +124,12 @@ $(document).ready(function(){
         }
         else if (e.keyCode === 37){
             tetris.move('left');
+        }
+        else if (e.keyCode === 40){
+            tetris.move('down');
+        }
+        else if(e.keyCode === 38){
+            tetris.rotate();
         }
     })
 }) 
